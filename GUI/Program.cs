134906f59987ad1a2730a16b53cd3e5009b88c0b
Erre <dalejandropacheco@unicesar.edu.co>
2024-11-25 13:18:05 -0500

    Mas y mas cambios

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new VistaConsulPrgRp());
+            Application.Run(new VistaEliminarUsuarios());
         }
     }
 }
---
+++ GUI/VistaEliminarUsuarios.cs
@@ -0,0 +1,34 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class VistaEliminarUsuarios : MaterialForm
+    {
+        public VistaEliminarUsuarios()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.BlueGrey800,
+                Primary.BlueGrey900,
+                Primary.BlueGrey500,
+                Accent.LightBlue200,
+                TextShade.WHITE
+            );
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
